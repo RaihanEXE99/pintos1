311b3a6c949d870a05be8862a09d079d9c16f00d
RaihanEXE99 <RaihanEXE99#gmail.com>
2023-07-11 21:32:44 +0600

    collegetashe

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -5,7 +5,7 @@
 #include "threads/thread.h"
 
 static void syscall_handler (struct intr_frame *);
-
+unsigned sys_tell(int fd);
 void
 syscall_init (void) 
 {
@@ -16,6 +16,41 @@ syscall_init (void)
 static void
 syscall_handler (struct intr_frame *f UNUSED)
 {
+  switch (syscall_number) {
+  case SYS_TELL: 
+    {
+      int fd;
+      unsigned return_code;
+
+      memread_user(f->esp + 4, &fd, sizeof(fd));
+
+      return_code = sys_tell(fd);
+      f->eax = (uint32_t) return_code;
+      break;
+    }
+
+  default:
+    printf("system call %d failed!\n", syscall_number);
+
+    sys_exit(-1);
+    break;
+  }
+
   printf ("system call!\n");
   thread_exit ();
+}
+
+unsigned sys_tell(int fd) {
+  lock_acquire (&filesys_lock);
+  struct file_desc* file_d = find_file_desc(thread_current(), fd, FD_FILE);
+
+  unsigned ret;
+  if(file_d && file_d->file) {
+    ret = file_tell(file_d->file);
+  }
+  else
+    ret = -1;
+
+  lock_release (&filesys_lock);
+  return ret;
 }
